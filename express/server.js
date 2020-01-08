1a79e6c808db61e8c7512b4ec04ca0644654481d
Alif Abhiesa <abhiesa24@gmail.com>
2020-01-08 21:14:14 +0700

    last

--- express/server.js
+++ express/server.js
@@ -23,9 +23,10 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 var routes = require('./routes');
 
+routes(app);
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
-module.exports.handler = serverless(app);
+exports.handler = serverless(app);
