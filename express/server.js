1b163e86fe93a3e213f54b7f7c72035589bd1ed8
Alif Abhiesa <abhiesa24@gmail.com>
2020-01-08 20:31:21 +0700

    ok

--- express/server.js
+++ express/server.js
@@ -1,6 +1,5 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
     bodyParser = require('body-parser'),
     controller = require('./controller');
 
@@ -13,5 +12,4 @@ app.use(bodyParser.json());
 var routes = require('./routes');
 routes(app);
 
-app.listen(port);
-console.log('Learn Node JS With Kiddy, RESTful API server started on: ' + port);
+module.exports = app;
